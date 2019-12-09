bb1311b1bddbc2556dabcc632c0408322b4cfa8f
魏明威 <txsb0000@126.com>
2019-12-09 18:24:16 +0800

    test

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,8 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <el-row>
+      <el-col :span="24" style="background: red;height: 20px"></el-col>
+    </el-row>
   </div>
 </template>
