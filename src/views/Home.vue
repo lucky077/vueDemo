6131ecede9f898c361183cfcd5df2a8fa56be393
魏明威 <txsb0000@126.com>
2019-12-31 15:51:47 +0800

    test

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,29 @@
 <template>
   <div style="width: 100%">
-    <el-input v-model="f1" @blur="getData" placeholder="请输入关键字" prefix-icon="el-icon-edit"></el-input>
+    <el-row>
+      <el-col :xl="12" :xs="18">
+        <el-input v-model="f1" @blur="getData" placeholder="请输入关键字" prefix-icon="el-icon-edit" @keydown.enter.native="getData"></el-input>
+      </el-col>
+      <el-col :xl="2" :xs="2" style="text-align: start">
+        <el-button @click="getData">搜索</el-button>
+      </el-col>
+    </el-row>
     <el-table :data="records" style="width: 100%" v-loading="loading">
       <el-table-column prop="account" label="帐号"> </el-table-column>
       <el-table-column prop="remark" label="备注"> </el-table-column>
       <el-table-column prop="aid" label="数字id"> </el-table-column>
       <el-table-column prop="card" label="身份证"> </el-table-column>
+      <el-table-column label="操作">
+        <template slot-scope="scope">
+          <el-button
+                  size="mini"
+                  @click="handleEdit(scope.row)">编辑</el-button>
+          <el-button
+                  size="mini"
+                  type="danger"
+                  @click="handleCheck(scope.row)">检查</el-button>
+        </template>
+      </el-table-column>
     </el-table>
     <el-col :xl="{span:6,offset:18}" :xs="{span:24,offset:0}" style="margin-top: 0.5%">
       <el-pagination
@@ -46,6 +64,18 @@ export default {
           this.copy(res,this)
           this.loading = false;
         });
+    },
+    handleCheck(row){
+      this.http.post("api/proxy",{url:"https://passport.jumpw.com/user.do"
+      ,body:{
+          dotype:0,
+          account:row.account,
+          idcard:row.card
+        }
+      })
+      .then(res => {
+        window.console.log(res)
+      })
     },
     handleSizeChange(val) {
       localStorage['pageSize'] = this.size = val
